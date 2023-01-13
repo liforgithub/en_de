a23ed00eb86265abe9bc05994e593abb4233d3db
lixueyang <lixueyang@znlh.com>
2023-01-13 17:24:53 +0800

    y

--- decrypt.py
+++ decrypt.py
@@ -7,11 +7,11 @@ with open('总字符.txt', encoding='utf-8') as f:
     print('加载完毕：' + str(len(all_chars)))
 
 while True:
-    need_encrypt_str = input('请输入要加密的段落： ')
+    need_encrypt_str = input('请输入要解密的段落： ')
 
     decrypt_code = []
-    for s in need_encrypt_str:
-        pos = all_chars.index(s)
-        decrypt_code.append(str(pos))
+    for s in need_encrypt_str.split(' '):
+        v = all_chars[int(s)]
+        decrypt_code.append(v)
 
-    print(' '.join(decrypt_code))
+    print(''.join(decrypt_code))
